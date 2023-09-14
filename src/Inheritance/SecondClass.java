d199d00b102b703b9cd2b82315c82306b0089517
firoz <sfirozhussain2@gmail.com>
2023-09-14 21:32:57 +0530

    commit3

--- src/Inheritance/SecondClass.java
+++ src/Inheritance/SecondClass.java
@@ -4,7 +4,7 @@ public class SecondClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi firoz");
+		System.out.println("hi firoz shaik");
 
 	}
 
