3abef5e8e33ec4d4c754c313d107e710c8d50232
hussain <sfirozhussain2@gmail.com>
2023-09-15 10:05:12 +0530

    hhh

--- src/OverLoading/MethodOverLoading.java
+++ src/OverLoading/MethodOverLoading.java
@@ -1,9 +1,47 @@
 package OverLoading;
 
+   class Ab{
+	   int a ;
+	   int b;
+	   
+	   void display(int a, int b) {
+		   this.a=a;
+		   this.b=b;
+		   
+	   }
+   
+   void sum() {
+	   System.out.println(a+b);
+   }
+   }
+   class Bb{
+	   int x;
+	   String na;
+	   void display(int x, String na) {
+		   this.x=x;
+		   this.na=na;
+	   }
+	   void show() {
+		   System.out.println(x);
+		   System.out.println(na);
+	   }
+   }
+
+
+
 public class MethodOverLoading {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//OverLoading o=new OverLoading();
+		Ab ab=new Ab();
+		ab.a=10;
+		ab.b=20;
+		ab.sum();
+		
+		Bb b=new Bb();
+		b.x=40;
+		b.na="hello";
+		b.show();
 		
 
 	}
